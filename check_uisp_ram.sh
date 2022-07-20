5bb7ec6247b0b4ed7e18a0409b7b3cdb45273211
rich568117 <rich568117@gmail.com>
2022-07-20 09:07:32 -0700

    Create check_uisp_ram.sh

---
+++ check_uisp_ram.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+URL="https://asd-uisp.asd103.org/nms/api/v2.1/devices/$1"
+curl --insecure -sS -X GET $URL -H "accept: application/json" -H "x-auth-token: 460d3a3f-1d99-4c00-a3ff-9e7c81800eae" | jq -r '.overview.ram' > unmsrawoutput.txt
+curl --insecure -sS -X GET $URL -H "accept: application/json" -H "x-auth-token: 460d3a3f-1d99-4c00-a3ff-9e7c81800eae" | jq -r '.identification.name' > unmsname.txt
+
+#result=$(cat unmsrawoutput.txt)
+devicename=$(cat unmsname.txt)
+result=$(($(cat unmsrawoutput.txt) +0))
+echo $result
+#echo $devicename
+
+if [[ $result -gt $2 ]]
+then
+echo "CRITICAL - $devicename has high RAM usage."
+ exit 3
+
+else
+echo "OK - $devicename RAM usage is over threshold."
+ exit 0
+
+ fi
